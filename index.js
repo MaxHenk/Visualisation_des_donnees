59057a86e9f67aaa29cc04525067961336030678
MaxHenk <max@henking.ch>
2022-05-14 11:28:07 +0200

    Changes
    Add communes and map and clean up of git

--- index.js
+++ index.js
@@ -29,25 +29,5 @@ var baseLayers = {
 var overlays = {};
 L.control.layers(baseLayers, overlays).addTo(mymap);
 
-// The svg
-var svg = d3.select("svg"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
-// Map and projection
-var path = d3.geoPath();
-var projection = d3.geoMercator()
-  .scale(70)
-  .center([0,20])
-  .translate([width / 2, height / 2]);
 
-// Data and color scale
-var data = d3.map();
-var colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(d3.schemeBlues[7]);
-
-// Load external data
-d3.queue()
-  .defer(d3.json, "Data/communesFR.geojson")
-  .defer(d3.csv, "https://raw.githubusercontent.com/MaxHenk/Visualisation_des_donnees/main/Data/premier_tour_clean.csv", function(d) { data.set(d.code, +d.pop); })
-  .await(ready);
+L.geoJSON(communes).addTo(mymap);
