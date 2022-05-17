bb8bf3dedcef4e2a84d1a50c041e94c765ebc29a
MaxHenk <max@henking.ch>
2022-05-17 20:05:40 +0200

    added interaction between csv and map layer

--- index.js
+++ index.js
@@ -29,21 +29,46 @@ var baseLayers = {
 var overlays = {};
 L.control.layers(baseLayers, overlays).addTo(mymap);
 
-function onEachFeature(feature, layer) {
-  if (feature.properties && feature.properties.libgeo){
-    layer.bindPopup(feature.properties.libgeo);
-  }
-}
-L.geoJSON(communes, {
-    onEachFeature: onEachFeature
-}).addTo(mymap);
+// function onEachFeature(feature, layer) {
+//   if (feature.properties && feature.properties.libgeo){
+//     layer.bindPopup(feature.properties.libgeo);
+//   }
+// }
+L.geoJSON(communes).addTo(mymap);
 
+
+// Papa.parse("https://raw.githubusercontent.com/MaxHenk/Visualisation_des_donnees/main/Data/premier_tour_clean.csv", {
+//     download: true,
+//     header: true,
+//     complete: function(results) { 
+//       console.log("Finished:", results.data);
+//     }
+// });
 
 Papa.parse("https://raw.githubusercontent.com/MaxHenk/Visualisation_des_donnees/main/Data/premier_tour_clean.csv", {
     download: true,
     header: true,
-    complete: function(results) { 
-      console.log("Finished:", results.data);
-    }
-});
+    complete: function(results) { //everything below runs only after the CSV has been loaded and processed.
+        communesHolder = L.geoJson(communes, { //defining the leaflet overlay layer that will house this data; polygons with location data is are already loaded in PuneElectoralWards variable.
+                //style: areastyle,
+                onEachFeature: function PBpopulate(feature, layer) { //below code will be executed for each item in the layer
+                    var popupText = "";
+                    var labelText = "";
+                    if (feature.properties && feature.properties.libgeo) {
+                        popupText += "<b>" + feature.properties.libgeo.toString() + "</b>";
+                        labelText = feature.properties.libgeo.toString();
+                        //Here it comes! The part where we look up the CSV data for a row having the same name as the map item's name
+                        var filtered = results.data.filter(function(data){return data.Commune == this;}, feature.properties.libgeo.toString());
+                        console.log(JSON.stringify(filtered));
+                        popupText += "<br>" + JSON.stringify(filtered);
+                        }
+                    if (feature.properties && feature.properties.description) {
+                        popupText += "<br>" + feature.properties.description.toString();
+                        }
+                    layer.bindPopup(popupText); //.bindLabel(labelText)
+                }   //closing onEachFeature function
+            }).addTo(mymap); //map layer created, added to "wards" overlay layer
+    }   //Papa.parse's complete: section over
+}); // Papa.parse function over.
+
 
