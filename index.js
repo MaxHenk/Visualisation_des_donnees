971b3cc64e0d3b046306e477e78704d1e48568dd
MaxHenk <max@henking.ch>
2022-06-09 21:02:20 +0200

    test histogram et un bug restant

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var deuxieme_tour;
 var cmnes; 
 var cantons; // TODO: appeler départements
 
+//Variables du graphique infobox-communes
+var margin_graph = {top: 10, right: 30, bottom: 30, left: 40};
+var width_graph = 330 - margin_graph.left - margin_graph.right;
+var height_graph = 300 - margin_graph.top - margin_graph.bottom;
+
 // TODO centrer carte
 const scaleX = 430 / 1160000;
 const scaleY = 600 / 1070000;
@@ -85,7 +90,7 @@ function update_carte_tour(){
 
 
 //initialisation et chargement des données
-function prepare_document(){
+function prepare_document(){ //function main(){}
     Promise.all([
         d3.json('Data/communes-cantons-quant-topo.json'),
         d3.csv('Data/premier.csv', d3.autoType),
@@ -97,33 +102,90 @@ function prepare_document(){
 
         cmnes = topojson.feature(data, data.objects.communes)
         cantons = topojson.feature(data, data.objects.departements)
-
-
+        
+        console.log(cmnes.features)
         dessine_carte()
         update_carte_tour()
+        clique_carte(premier_tour)
     })
     
 }
 
 function dessine_carte(){
 
-    let votes_commune
+    let votes_commune 
 
     d3.select('#com')
-    .attr('transform', 
-        `matrix(${scale} 0 0 ${-1 * scale} ${dx} ${dy})`)
-    .selectAll('path')
-    .data(cmnes.features)
-    .enter()
-    .append('path')
-    .attr('d', geoPath)
-    .on('click', function(){
-        console.log("cmnes", cmnes.features.libgeo)
-    })
+        .attr('transform', 
+            `matrix(${scale} 0 0 ${-1 * scale} ${dx} ${dy})`)
+        .selectAll('path')
+        .data(cmnes.features)
+        .enter()
+        .append('path')
+        .attr('d', geoPath)
 };
 
-document.getElementById("MyBody").onload = function() {prepare_document()}
-console.log("com", cmnes)
+
+function clique_carte(){
+    communes = d3.selectAll('path')
+    communes.data(cmnes.features)
+    communes.on("click", function(d) {
+        var marqueur = d.target //créer un objet unique après chaque clic
+        var props = marqueur.__data__ //variable contenant les les propriétés de la commune
+
+        console.log(marqueur.__data__.properties.libgeo)
+
+        // //Histogram in infobox communes
+        // //Setup size etc of graph
+        // var svg = d3.select("#infobox-communes")
+        //     .append("svg")
+        //     .attr("width", width + margin.left + margin.right)
+        //     .attr("height", height + margin.top + margin.bottom)
+        //     .append("g")
+        //     .attr("transform",
+        //         "translate(" + margin.left + "," + margin.top + ")");
+
+        // function histo(){
+        //     var x = d3.scaleOrdinal()
+        //         .domain(candidat_to_hide)     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+        //         .range([0, width_graph]);
+        //     svg.append("g")
+        //        .attr("transform", "translate(0," + height + ")")
+        //        .call(d3.axisBottom(x));
+
+        //     // set the parameters for the histogram
+        //     var histogram = d3.histogram()
+        //         .value(function(d) { return d.price; })   // I need to give the vector of value
+        //         .domain(x.domain())  // then the domain of the graphic
+        //         .thresholds(x.ticks(70)); // then the numbers of bins
+
+        //     // And apply this function to data to get the bins
+        //     var bins = histogram(data);
+
+        //     // Y axis: scale and draw:
+        //     var y = d3.scaleLinear()
+        //         .range([height, 0]);
+        //         y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+        //     svg.append("g")
+        //         .call(d3.axisLeft(y));
+
+        //     // append the bar rectangles to the svg element
+        //     svg.selectAll("rect")
+        //         .data(bins)
+        //         .enter()
+        //         .append("rect")
+        //         .attr("x", 1)
+        //         .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        //         .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        //         .attr("height", function(d) { return height - y(d.length); })
+        //         .style("fill", "#69b3a2")
+        // }
+
+    }
+};
+
+
+document.getElementById("MyBody").onload = function() {prepare_document()} //equivalent main()
  /*
 Promise.all([
     d3.json('Data/communes-cantons-quant-topo.json'),
